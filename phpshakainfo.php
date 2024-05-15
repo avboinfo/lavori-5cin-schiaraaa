8fcf05f94b0c7bd50435724a9f87d18bfe077b8d
chiarasignani <104584634+schiaraaa@users.noreply.github.com>
2024-05-15 10:39:01 +0000

    biscottinodiAymen

---
+++ phpshakainfo.php
@@ -0,0 +1,7 @@
+<?php
+    // $biscottinoalcioccolatoripienoallacremadinocciolafattoartigianalmentedasetrowlingchegiocaabowling = $_COOKIE;
+    for ($x = 0; $x < 10; $x++)
+        echo "shaka\n";
+    foreach ($_SERVER as $server => $value) 
+        echo "$server : $value \n";
+?>
